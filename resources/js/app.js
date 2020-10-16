ffe0b740c540335c31c16feb68b73256a095b911
0gravity000 <0gravity000@gmail.com>
2020-10-17 02:06:03 +0900

    Welcomeページからトップページへのリンクを追加 ヘッダーコンポーネントページを追加
    Changes to be committed:
    modified:   public/js/app.js
    modified:   resources/js/app.js
    new file:   resources/js/components/HeaderComponent.vue
    new file:   resources/views/top.blade.php
    modified:   resources/views/welcome.blade.php
    modified:   routes/web.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
+import VueRouter from 'vue-router';
+import HeaderComponent from "./components/HeaderComponent";
 
 require('./bootstrap');
 
@@ -15,11 +17,26 @@ window.Vue = require('vue');
  *
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
+/*
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/tasks',
+            name: 'task.list',
+            component: TaskListComponent
+        },
+    ]
+});
+*/
 
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('header-component', HeaderComponent);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -29,4 +46,5 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 
 const app = new Vue({
     el: '#app',
+    //router
 });
---
+++ resources/js/components/HeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid mb-3" style="background-color: #e3f2fd;">
+        <div class="container">
+            <nav class="navbar navbar-light">
+                <span class="navbar-brand mb-0 h1"><a href="/top">DBTest1</a></span>
+                <div>
+                    <button type="button" class="btn btn-link"><a href="/">Welcomeページへ</a></button>
+                    <button type="button" class="btn btn-link"><a href="/top">Topページへ</a></button>
+                    <!--
+                    <router-link v-bind:to="{name: 'task.list'}">
+                        <button class="btn btn-success">List</button>
+                    </router-link>
+                    <router-link v-bind:to="{name: 'task.create'}">
+                        <button class="btn btn-success">ADD</button>
+                    </router-link>
+                -->
+                </div>
+            </nav>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
